4ac02b2f8f32dd4d85c2dea034e7c31c27d0da69
David Hazel <david.hazel@oregon.gov>
2020-02-25 08:44:00 -0800

    Successfully reproduces the bug

---
+++ api/Controllers/EchoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dto.Sort;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EchoController : ControllerBase
+    {
+        private readonly ILogger<EchoController> _logger;
+
+        public EchoController(ILogger<EchoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Sort> Get([FromQuery] List<Sort> sort)
+        {
+            return sort;
+        }
+    }
+}
---
+++ api/Dto/Sort.cs
@@ -0,0 +1,9 @@
+
+namespace api.Dto.Sort
+{
+    public class Sort
+    {
+        public string Field { get; set; }
+        public string Dir { get; set; }
+    }
+}
